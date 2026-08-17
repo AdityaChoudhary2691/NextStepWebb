40f013636081e19c975e0dd0300095c2ef80bee8
aditya <adityachoudharyofficial29@gmail.com>
2026-08-17 06:58:33 +0530

    data fetching from database

---
+++ NexepBackend/src/main/java/com/aditya/nexepbackend/NexepBackendApplication.java
@@ -0,0 +1,12 @@
+package com.aditya.nexepbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexepBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexepBackendApplication.class, args);
+    }
+}
---
+++ NexepBackend/src/test/java/com/aditya/nexepbackend/NexepBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aditya.nexepbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexepBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
